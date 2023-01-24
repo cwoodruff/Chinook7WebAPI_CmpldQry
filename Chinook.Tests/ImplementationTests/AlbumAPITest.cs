d94ae6d8225e236d1a5a7988f5477960891c7e43
Chris Woodruff <cwoodruff@live.com>
2023-01-24 13:22:26 -0500

    added tests and update NuGet packages

---
+++ Chinook.Tests/ImplementationTests/AlbumAPITest.cs
@@ -0,0 +1,56 @@
+﻿using System.Net;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+
+namespace Chinook.Tests.ImplementationTests;
+
+public class AlbumApiTest : IDisposable
+{
+    private readonly HttpClient _client;
+
+    public AlbumApiTest()
+    {
+        var application = new WebApplicationFactory<Program>()
+            .WithWebHostBuilder(builder =>
+            {
+                // ... Configure test services
+            });
+
+        _client = application.CreateClient();
+    }
+    
+    public void Dispose()
+    {
+        _client.Dispose();
+    }
+
+    [Theory]
+    [InlineData("GET")]
+    public async void AlbumGetAllTest(string method)
+    {
+        // Arrange
+        var request = new HttpRequestMessage(new HttpMethod(method), "/api/Album/");
+
+        // Act
+        var response = await _client.SendAsync(request);
+
+        // Assert
+        response.EnsureSuccessStatusCode();
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+    }
+
+    [Theory]
+    [InlineData("GET", 1)]
+    public async Task AlbumGetTest(string method, int? id = null)
+    {
+        // Arrange
+        var request = new HttpRequestMessage(new HttpMethod(method), $"/api/Album/{id}");
+
+        // Act
+        var response = await _client.SendAsync(request);
+
+        // Assert
+        response.EnsureSuccessStatusCode();
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+    }
+}
